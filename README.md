66f77622024837b96afb953b84027e78df6047cd
Luigi De Rosa <lurukee@gmail.com>
2016-04-22 12:28:40 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # on-appear
+
+Work in progress.
+
+Give an HTMLElement, and will execute a callback as soon the element comes in the viewport.
+
---
+++ index.js
@@ -0,0 +1,50 @@
+var OnAppear = function(el, opts) {
+  this.el = el;
+  this.vars = this.extend({
+    delay: 0, // ms of delay for the callback
+    offset: 0, // px of offset
+    callback: function(){}
+  }, opts);
+
+  this._done = false;
+  this.bind();
+  this.check();
+};
+
+OnAppear.prototype.extend = function(obj, props) {
+  var newObj = Object.create(obj);
+
+  for(var prop in props) {
+    if(props.hasOwnProperty(prop)) {
+      newObj[prop] = props[prop];
+    }
+  }
+
+  return newObj;
+};
+
+OnAppear.prototype.bind = function() {
+  this.checkFn = this.check.bind(this);
+  window.addEventListener('scroll', this.checkFn);
+};
+
+OnAppear.prototype.destroy = function() {
+  this._done = true;
+  window.removeEventListener('scroll', this.checkFn);
+};
+
+OnAppear.prototype.isInViewport = function() {
+  const rect = this.el.getBoundingClientRect();
+  return rect.top + rect.height > 0 && (rect.top - this.vars.offset) < window.innerHeight;
+};
+
+OnAppear.prototype.check = function() {
+  if (this._done || !this.isInViewport()) {
+    return;
+  }
+
+  this.destroy();
+  window.setTimeout(this.vars.callback, this.vars.delay);
+};
+
+module.exports = OnAppear;
